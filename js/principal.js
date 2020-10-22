9b8624c4c842514b5883b330b6d064306c121962
Angel <69370491+angtor-dev@users.noreply.github.com>
2020-10-22 00:31:54 -0400

    Pequeños cambios
    Se añadió el nombre SICU al inicio de sesión, se termino de aplicar estilos al formulario de usuarios, se añadió una función de javascript para volver atrás en el boton "cancelar" de los formularios y se cambio la direccion del archivo .css del tema original porque daba un error en la consola

--- js/principal.js
+++ js/principal.js
@@ -1,5 +1,9 @@
-window.onload = init //Se ejecita la función init() al terminar de carga la pagina
+window.onload = init; //Se ejecita la función init() al terminar de carga la pagina
 
+// Variables:
+var estado_temas = false; //variable para saber el estado del menú "temas", por defecto cerrado
+
+// Funciones:
 function init() { //función que se ejecuta justo despues de cargar la pagina
 	add_estilos();
 	add_eventos();
@@ -12,15 +16,24 @@ function add_estilos() {
 	for(var i=0, l=botones.length; i<l; i++){
 		botones[i].style.transition = ".4s";
 	}
-	//Modifica la transición del botón de perfil cuando se clickea
-	document.getElementById("btn-perfil").onmousedown = function() {this.style.transition = "0s";}
-	document.getElementById("btn-perfil").onmouseleave = function() {this.style.transition = ".4s";}
 }
 
 //Función para añadir eventos a los elementos desde js (para que el html quede más entendible)
 function add_eventos() {
 	document.getElementById('btn-perfil').addEventListener("click", abrir_menu);
 	document.querySelector('.menu-perfil .header .fa-times').addEventListener("click", cerrar_menu);
+	document.querySelector('.boton.temas').addEventListener("click", toggle_temas);
+	//Añadir evento para cambiar tema dependiendo del id de cada botón en el menú
+	var btn_temas = document.querySelectorAll('.boton.tema');
+	for (var i=0; i<btn_temas.length; i++) {
+		btn_temas[i].addEventListener("click", function() {
+			cambiar_tema("css/temas/" + this.getAttribute('id') + ".css")
+		})
+	}
+	//añade la funcion para regresar en el boton "cancelar" de los formularios, si existen
+	if (document.querySelector('.contenido .cuerpo form div .boton.cancelar')) {
+		document.querySelector('.contenido .cuerpo form div .boton.cancelar').addEventListener("click", function(){cancelar_registro(this.getAttribute('value'))})
+	}
 }
 
 //Función para abrir Menú de Perfil
@@ -36,5 +49,30 @@ function cerrar_menu() {
 	document.querySelector('.contenedor-menu').style.backgroundColor = "rgba(0, 0, 0, .0)";
 	document.querySelector('.menu-perfil').style.width = "0";
 	document.body.style.overflowY = "auto";
-	setTimeout(function() {document.querySelector('.contenedor-menu').style.width = "0";},400);
+	setTimeout(function() {document.querySelector('.contenedor-menu').style.width = "0";}, 400);
+	//Cerrar submenus si estan abiertos
+	if (estado_temas) {
+		document.querySelector('.contenedor-temas').style.height = "0";
+		estado_temas = false;
+	}
+}
+
+// Función para abrir y cerrar el submenu de los temas
+function toggle_temas() {
+	if (estado_temas == false) {
+		document.querySelector('.contenedor-temas').style.height = "auto";
+		estado_temas = true;
+	} else {
+		document.querySelector('.contenedor-temas').style.height = "0";
+		estado_temas = false;
+	}
+}
+
+//Función para cambiar el tema (color) del programa
+function cambiar_tema(tema) {
+	document.getElementById('tema').href = tema;
+}
+
+function cancelar_registro(pagina) {
+	window.location.href = pagina + ".html";
 }
